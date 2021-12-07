d575c193db8763e5329230e57b14397e1902f58a
Lev <lev.petryakov@gmail.com>
2021-12-08 00:21:02 +0200

    Add cors config

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	userEndpoints "warehouse/routes/user"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	router.Use(cors.New(config))
 	userEndpoints.Activate(router)
 	router.Run()
 }
